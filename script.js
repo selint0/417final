5b9d5642928a4a0e78e274d0b9be4cfbdb3b3808
selint0 <selint@asu.edu>
2024-04-21 11:54:18 -0700

    updated pictures in html and then added to css

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+// ------------------------------------------------------
+// Light/Dark Mode Function
+// ------------------------------------------------------
+// The easiest way to write this code is to have a class that you add or remove from the body of your HTML. This function should be called when the user clicks on the control you use for light/dark mode (usually a checkbox or button)
+// TO DO
+
+// When the function is called, check to see if the body tag has the "dark" class on it (you can name this class what you want, and can use the classList.contains() method to check https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/contains)
+
+// If the dark class is there, remove it 
+
+// if the dark class isn't there, add it
+
+// in both of those cases you could also update the icon you display on your button (if you have one), but it's not required
+
+
+/* ------------------------------------------------------
+HOW THIS WORKS:
+if you add and remove the class from the body and write your styles so that they change depending on whether or not the class is on the body, you'd need to write styles similar to this:
+
+FOR LIGHT MODE
+a{
+		styles
+}
+
+FOR DARK MODE
+body.dark a{
+		styles
+}
+
+// ------------------------------------------------------
+*/ 
+
+
+// ------------------------------------------------------
+// EVENT HANDLERS
+// ------------------------------------------------------
+// attach event listener for light/dark mode
+// TO DO
+// ------------------------------------------------------
